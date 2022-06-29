afbba76ca157ef9177857e630561c0c33b388015
tobyProctor <104203955+shouedpro@users.noreply.github.com>
2022-06-29 13:49:25 +0100

    added phonetics file

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-        {props.synonyms.map(function (synonyms, index) {
-          return <li key={index}>{synonyms}</li>;
+      <div className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <span key={index}>{synonym}</span>;
         })}
-      </ul>
+      </div>
     );
   } else {
     return null;
