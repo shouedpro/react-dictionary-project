dd4372d38950265ec94002109806725a66c96677
tobyProctor <104203955+shouedpro@users.noreply.github.com>
2022-06-29 13:11:08 +0100

    added synonyms file

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -10,6 +11,7 @@ export default function Meaning(props) {
           <div key={index}>
             <p className="definition">{definition.definition}</p>
             <p className="example">{definition.example}</p>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
