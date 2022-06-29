c7d65fa0f73c7abbfa762edee102fdba1fdde915
tobyProctor <104203955+shouedpro@users.noreply.github.com>
2022-06-29 16:35:01 +0100

    additional tweaks in css

--- src/Results.js
+++ src/Results.js
@@ -8,7 +8,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <section>
-          <h2>{props.results.word}</h2>
+          <h2 className="search-word">{props.results.word}</h2>
           {props.results.phonetics.map(function (phonetic, index) {
             return (
               <div key={index}>
