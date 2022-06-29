e551ede1866975fa858b454f59c188d70ace726b
tobyProctor <104203955+shouedpro@users.noreply.github.com>
2022-06-29 12:03:52 +0100

    added meaning.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -25,6 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Meaning.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Meaning() {
+  return <div className="Meaning">Hello from meaning</div>;
+}
